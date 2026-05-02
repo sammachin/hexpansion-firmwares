fed914ef0df3a63d45a42363a9529d67fcf66be8
Matthew Wilkes <git@matthewwilkes.name>
2026-05-02 18:28:15 +0100

    Add UK map hexpansion placeholder

---
+++ 0x4291/0x1830/app.py
@@ -0,0 +1,27 @@
+import app
+import neopixel
+import asyncio
+from patterns.rainbow import RainbowPattern
+
+
+class RainbowUK(app.App):
+    def __init__(self, config=None):
+        self.config = config
+        self.leds = neopixel.NeoPixel(config.pin[3], 78)
+        self.pattern = RainbowPattern()
+
+
+    def update(self, delta=None):
+        self.minimise()
+
+    async def background_task(self):
+        while True:
+            frame = self.pattern.next()
+            for i, val in enumerate(frame*6):
+                if i > 78:
+                    break
+                self.leds[i] = tuple(int(c * 0.1) for c in val)
+            self.leds.write()
+            await asyncio.sleep(1/self.pattern.fps)
+
+__app_export__ = RainbowUK
